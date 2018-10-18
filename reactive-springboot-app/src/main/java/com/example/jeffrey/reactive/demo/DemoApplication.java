079d6e5aa530f5b7ae8005f37217811db43530e5
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2018-10-19 00:00:18 +0800

    fix typo

--- reactive-springboot-app/src/main/java/com/example/jeffrey/reactive/demo/DemoApplication.java
+++ reactive-springboot-app/src/main/java/com/example/jeffrey/reactive/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -21,11 +22,18 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
+	public WebMvcConfigurer initializrWebMvcConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("*");
+			}
+
+			// Spring Boot 2 moves actuator endpoints to /actuator and we want
+			// to follow that default by adding a simple redirect
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addRedirectViewController("/info", "/actuator/info");
 			}
 		};
 	}
